54f75b0775c060fa8d3a4b826379023f3485fba2
MariaPSegarra <mariapsegarra@gmail.com>
2020-11-23 20:54:40 +0000

    Created Add in heroku and moved database

--- ecommerce_django/settings.py
+++ ecommerce_django/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,9 +133,9 @@ WSGI_APPLICATION = 'ecommerce_django.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+  'default': {
+      'ENGINE': 'django.db.backends.sqlite3',
+      'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
